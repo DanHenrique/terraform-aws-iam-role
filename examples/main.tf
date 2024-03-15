86d7375a0d77cc71dc59b98309c525bfd88c7a21
DanHenrique <daanielhenrique7@gmail.com>
2024-03-15 19:57:28 -0300

    feat: add release version

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
 module "iam_role_with_policies" {
-  source               = "github.com/DanHenrique/terraform-aws-lambda" # ?ref=v1.0.0
+  source               = "github.com/DanHenrique/terraform-aws-lambda?ref=v1.0.0"
   role_name            = "ExampleRole"
   assume_role_policy_document = file("./role/role.json")
 
